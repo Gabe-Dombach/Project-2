deb05702bc09056c3bbb20e0a9d732c472b57eec
Gabriel-Dombach <gdombach009@gmail.com>
2021-11-09 15:32:54 -0500

    Added email to login and button to reset account

--- sign-in/Resources/login-script.js
+++ sign-in/Resources/login-script.js
@@ -1,10 +1,21 @@
 let userArr = [];
 $(document).ready(function(){
+    function isEmail(address){
+        address = address.split('')
+        if(address.indexOf('@') != -1 && address.indexOf('@') != 0 && address.indexOf('.' ) && address.indexOf('.') > address.indexOf('@')){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
   $('.submit').click(function(){
       let curretStorage = JSON.parse(localStorage.getItem('users'));
-        if(curretStorage == null){
+        if(curretStorage == null && isEmail($('#email').val())){
             const newUser = {
                 userName :$('#username').val(),
+                email:$('#email').val(),
                 winsTic:0,
                 loseTic:0,
                 winsRPS:0,
@@ -14,6 +25,9 @@ $(document).ready(function(){
         }
         else if($('#username').val() == curretStorage.userName){
             window.location.replace('../main_page.html');
+        }
+        else if(!isEmail($('#email').val())){
+            window.alert('Please enter a valid email adress!')
         }
         else{
             window.alert('INCORRECT CODE')
---
+++ sign-in/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.reset').click(function(){
+        localStorage.removeItem('users');
+        window.location.replace('sign-in/Up.html')
+    })
+})
