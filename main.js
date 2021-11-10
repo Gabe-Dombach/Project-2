4985c92d28197ded4b5eadac3e81c02ca1cfbc40
Jeremy Kropa <arkedi487@gmail.com>
2021-11-10 07:49:57 -0500

    yes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ 
 window.onload = function() { 
     var name = localStorage.getItem('username');
     if(name != "undefined" || name != "null") { 
@@ -6,7 +6,6 @@ window.onload = function() {
     }
 }
 a=JSON.parse((localStorage.getItem("users")));
-a.push({name: username, password: password});
 localStorage.setItem('currentUser',JSON.stringify(a));
 for(var i=0; i<a.length; i++)
   {
@@ -14,4 +13,3 @@ for(var i=0; i<a.length; i++)
    li.innerHTML=a[i]['name'];
    document.getElementById("listuser").appendChild(li);
 }
-console.log("gabe is gey")
